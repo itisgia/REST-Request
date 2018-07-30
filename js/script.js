1b8befc8b684ddb259d12f141e1cd69a2a0c1322
Jiah <itisgia@gmail.com>
2018-07-31 11:50:33 +1200

    error

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $.ajax({
     type: "GET",
     success: function functionName(data) {
       key = data["0"].API_KEY;
-      getData(1);
+      // getData(1);
     },
     error : function functionName(error) {
       console.log("ERROR");
@@ -31,9 +31,28 @@ function getData(minAge) {
     })
 }
 
+function getCurrencyCode(currency) {
+    $.ajax({
+      //min age and key parameters what's going to help us built us up
+        url: "https://my.api.mockaroo.com/peopleData.json?"+money_symbol+"&key="+key,
+        dataType: "json",
+        type: "GET",
+        success: function functionName(mockerooData) {
+          console.log(mockerooData);
+        },
+        error : function functionName(error) {
+          console.log("ERROR");
+          console.log(error);
+        }
+    })
+}
 
 $("#ageForm").submit(function functionName() {
     event.preventDefault();
     var minAge = $("input[name='minAge']").val();
+    var currency = $("#selectWrapper")
     getData(minAge);
 })
+
+var currency = $("#selectWrapper");
+console.log(currency);
