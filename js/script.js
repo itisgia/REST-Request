712efa3a143f04dd77dffbfcbf4564688b0479ac
Jiah <itisgia@gmail.com>
2018-07-31 10:38:22 +1200

    using mockeroo live data

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var key;
+$.ajax({
+    url: "config.json",
+    dataType: "json",
+    type: "GET",
+    success: function functionName(data) {
+      key = data["0"].API_KEY;
+      getData(1);
+    },
+    error : function functionName(error) {
+      console.log("ERROR");
+      console.log(error);
+    }
+});
+
+
+//can't start writing ajax again . ajaza is async.. so it  won't wait till other functuon wait. so best way is creat a functio
+function getData(minAge) {
+    $.ajax({
+      //min age and key parameters what's going to help us built us up
+        url: "https://my.api.mockaroo.com/peopleData.json?min_age="+minAge+"&key="+key,
+        dataType: "json",
+        type: "GET",
+        success: function functionName(mockerooData) {
+          console.log(mockerooData);
+        },
+        error : function functionName(error) {
+          console.log("ERROR");
+          console.log(error);
+        }
+    })
+}
+
+
+$("#ageForm").submit(function functionName() {
+    event.preventDefault();
+    var minAge = $("input[name='minAge']").val();
+    getData(minAge);
+})
